90c98b2d521c311f7e719717aaa51f1e61e2007d
Vinicius Catarino <vinicatarino@gmail.com>
2020-07-15 10:10:24 -0300

    Finalizando Spa com gulp

--- gulp/spa/gulpTasks/app.js
+++ gulp/spa/gulpTasks/app.js
@@ -1,23 +1,48 @@
 const gulp = require('gulp')
+const babel = require('gulp-babel')
+const uglify = require('gulp-uglify')
+const sass = require('gulp-sass')
+const uglifycss = require('gulp-uglifycss')
+const concat = require('gulp-concat')
+const htmlmin = require('gulp-htmlmin')
 
-function appHtml(cb) {
-    return cb()
+function appHtml() {
+    return gulp.src('src/**/*.html')
+        .pipe(htmlmin({collapseWhitespace: true}))
+        .pipe(gulp.dest('build'))
 }
 
-function appCss(cb) {
-    return cb()
+function appCss() {
+    return gulp.src('src/assets/sass/index.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(uglifycss({"uglyComments": true}))
+    .pipe(concat('app.min.css'))
+    .pipe(gulp.dest('build/assets/css'))
+
 }
 
 
-function appJS(cb) {
-    return cb()
+function appJS() {
+    return gulp.src('src/assets/js/**/*.js')
+        .pipe(babel({
+            comments: false,
+            presets: ["env"]
+        }))
+        .pipe(uglify())
+        .pipe(concat('app.min.js'))
+        .pipe(gulp.dest('build/assets/js'))
 }
 
 
-function appIMG(cb) {
-    return cb()
+function appIMG() {
+    return gulp.src('src/assets/imgs/**/*.*')
+    .pipe(gulp.dest('build/assets/imgs'))
 }
 
+gulp.task('appHtml', appHtml)
+gulp.task('appJs', appJS)
+gulp.task('appCss', appCss)
+gulp.task('appImg', appIMG)
 
 module.exports = {
     appHtml,
--- gulp/spa/gulpTasks/servidor.js
+++ gulp/spa/gulpTasks/servidor.js
@@ -1,14 +1,23 @@
 const gulp = require('gulp');
+const webserver = require('gulp-webserver')
+const watch = require('gulp-watch')
+function servidor(cb) {
+    return gulp.src('build')
+    .pipe(webserver({
+        port: 8080,
+        open: true,
+        livereload: true,
+    }))
+}
 
 
 function monitorarArquivos(cb) {
+    watch('src/**/*.html', () => gulp.series('appHtml')())
+    watch('src/**/*.scss', () => gulp.series('appCss')())
+    watch('src/**/*.js', () => gulp.series('appJs')())
+    watch('src/assets/imgs/**/*.*', () => gulp.series('appImg')())
     return cb()
 }
-
-function servidor(cb) {
-    return cb()
-}
-
 module.exports = {
     monitorarArquivos,
     servidor
